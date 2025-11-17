8d8ba955f4a0610cc632ebc307039e2dc4c84a86
momomak <m.makria@gmail.com>
2025-11-17 11:37:45 +0100

    adding method for switching between pages

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -3,4 +3,10 @@ class QuizQuestions {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffled = List.of(answers);
+    shuffled.shuffle();
+    return shuffled;
+  }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,22 +12,34 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuesttion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currenQuestion = questions[0];
+    final currenQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(currenQuestion.text),
-          SizedBox(height: 30),
-          ...currenQuestion.answers.map((anwser) {
-            return AnswerButton(anwser, () {});
-          }),
-          AnswerButton(currenQuestion.answers[0], () {}),
-        ],
+      child: Container(
+        margin: EdgeInsets.all(36),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            Text(currenQuestion.text, textAlign: TextAlign.center),
+            SizedBox(height: 30),
+            ...currenQuestion.getShuffledAnswers().map((anwser) {
+              return AnswerButton(anwser, answerQuesttion);
+            }),
+          ],
+        ),
       ),
     );
   }
